d4c50a7da056228d1dee2dde521a5e780ae3f27a
prashah <prashah@tibco.com>
2018-03-11 17:14:34 +0530

    js parser

--- activity.go
+++ activity.go
@@ -39,13 +39,14 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 	vm.Set("args", args)
 
 	if _, err := vm.Run(context.GetInput("jscode")); err == nil {
-		if argsOut1, err := vm.Get("args"); err == nil {
-			log.Debugf("The value of i is %s", argsOut1) // 4
-			// context.SetOutput("args_out", argsOut)
-			context.SetOutput("args_out", argsOut1)
+		if value, err := vm.Get("args"); err == nil {
+			if argsOut1, err := value.ToString(); err == nil {
+				log.Debugf("The value of i is %s", argsOut1) // 4
+				// context.SetOutput("args_out", argsOut)
+				context.SetOutput("args_out", argsOut1)
 
-			log.Debugf("The value of j is %s", argsOut1) // 4
-
+				log.Debugf("The value of j is %s", argsOut1) // 4
+			}
 		}
 	} else {
 		context.SetOutput("args_out", args)
