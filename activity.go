05bde372b0c515eb40f439cca5ecab444d63e922
prashah <prashah@tibco.com>
2018-03-11 15:48:52 +0530

    js parser

--- activity.go
+++ activity.go
@@ -34,11 +34,11 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 	// 	console.log("The value of abc is " + abc); // 4
 	// `)
 
-	args := context.GetInput("args").(string)
+	args := context.GetInput("args")
 
 	vm.Set("args", args)
 
-	code, _ := vm.Run(context.GetInput("code").(string))
+	//code, _ := vm.Run(context.GetInput("code").(string))
 	// Get the activity data from the context
 	// name := context.GetInput("name").(string)
 	// salutation := context.GetInput("salutation").(string)
@@ -48,12 +48,14 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 
 	// Set the result as part of the context
 	// if value, err := vm.Get("abc"); err == nil {
-	if argsOut, err := code.ToString(); err == nil {
-		log.Debugf("The value of i is [%s]", argsOut) // 4
-		context.SetOutput("args_out", argsOut)
-	}
+	// if argsOut, err := code.ToString(); err == nil {
+	// 	log.Debugf("The value of i is [%s]", argsOut) // 4
+	// 	context.SetOutput("args_out", argsOut)
+	// }
 
 	// }
+
+	context.SetOutput("args_out", "pranav")
 
 	// Signal to the Flogo engine that the activity is completed
 	return true, nil
